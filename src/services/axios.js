2a6125535b03db8fc241159d8bf364adb7136e53
williansoncini <willian_soncini@hotmail.com>
2022-03-22 22:48:54 -0300

    Subindo alterações

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
-  // baseURL: 'http://35.215.255.138',
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://localhost:3002',
 });
